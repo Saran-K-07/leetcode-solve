1ccd21ab598f5a9fdf21f9e29553fdac902cf893
Saran K <hiclashersaran@gmail.com>
2026-01-21 14:43:14 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,20 @@
+// Last updated: 21/01/2026, 14:43:14
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int[] freq = new int[26];
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            char h = t.charAt(i);
+            freq[c-'a']++;
+            freq[h-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0)
+            return false;
+        }
+        return true;
+    }
+}
